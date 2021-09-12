20537b8ee204f2ffeea52647d0e2857a7e79ff52
John Snow <e14036122@gmail.com>
2021-09-13 00:39:04 +0800

    Add new file

---
+++ reviews_data.py
@@ -0,0 +1,20 @@
+data = []
+count = 0
+
+with open('reviews.txt', 'r') as f:
+
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+
+print('Total', count,'data')
+
+sum_len = 0
+
+for dat in data:
+	length = len(dat)
+	sum_len = length + sum_len
+
+print('average length =', sum_len / count, 'data')
