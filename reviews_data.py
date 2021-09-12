30841789346c63555b3e65d70ab24285cd6fcce5
John Snow <e14036122@gmail.com>
2021-09-13 00:42:55 +0800

    calculate avg len

--- reviews_data.py
+++ reviews_data.py
@@ -15,6 +15,6 @@ sum_len = 0
 
 for dat in data:
 	length = len(dat)
-	sum_len = length + sum_len
+	sum_len = length + sum_len #也可以直接寫成 sum_len += len(dat)
 
 print('average length =', sum_len / count, 'data')
